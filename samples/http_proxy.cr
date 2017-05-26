7b6e38275402916dc8d3202424750279d8b7c00a
Theo Li <bbtfrr@gmail.com>
2017-05-26 20:37:52 +0800

    fix type missmatch

--- samples/http_proxy.cr
+++ samples/http_proxy.cr
@@ -1,8 +1,10 @@
 require "http/proxy"
 
 server = HTTP::Proxy.new(8080, handlers: [
-  HTTP::LogHandler.new
-])
+  HTTP::LogHandler.new,
+]) do |context|
+  puts context
+end
 
 puts "Listening on http://127.0.0.1:8080"
 server.listen
--- src/http/proxy.cr
+++ src/http/proxy.cr
@@ -3,16 +3,15 @@ require "http/server"
 require "./proxy/handler"
 
 class HTTP::Proxy < HTTP::Server
-
   def self.new(port)
     new("127.0.0.1", port)
   end
 
-  def self.new(port, &handler : Context ->)
+  def self.new(port, &handler : HTTP::Handler::Proc)
     new("127.0.0.1", port, &handler)
   end
 
-  def self.new(port, handlers : Array(HTTP::Handler), &handler : Context ->)
+  def self.new(port, handlers : Array(HTTP::Handler), &handler : HTTP::Handler::Proc)
     new("127.0.0.1", port, handlers, &handler)
   end
 
@@ -29,12 +28,12 @@ class HTTP::Proxy < HTTP::Server
     @processor = RequestProcessor.new handler
   end
 
-  def initialize(@host : String, @port : Int32, &handler : Context ->)
+  def initialize(@host : String, @port : Int32, &handler : HTTP::Handler::Proc)
     handler = HTTP::Proxy.build_middleware handler
     @processor = RequestProcessor.new handler
   end
 
-  def initialize(@host : String, @port : Int32, handlers : Array(HTTP::Handler), &handler : Context ->)
+  def initialize(@host : String, @port : Int32, handlers : Array(HTTP::Handler), &handler : HTTP::Handler::Proc)
     handler = HTTP::Proxy.build_middleware handlers, handler
     @processor = RequestProcessor.new handler
   end
@@ -49,20 +48,20 @@ class HTTP::Proxy < HTTP::Server
     @processor = RequestProcessor.new handler
   end
 
-  def self.build_middleware(handler : (Context ->)? = nil)
+  def self.build_middleware(handler : HTTP::Handler::Proc? = nil)
     proxy_handler = HTTP::Proxy::Handler.new
     proxy_handler.next = handler if handler
     proxy_handler
   end
 
-  def self.build_middleware(handlers : Array(HTTP::Handler), last_handler : (Context ->)? = nil)
+  def self.build_middleware(handlers, last_handler : HTTP::Handler::Proc? = nil)
     proxy_handler = build_middleware last_handler
     return proxy_handler if handlers.empty?
 
     handlers.each_cons(2) do |group|
       group[0].next = group[1]
     end
-    handlers.last.next = proxy_handler
+    handlers.last.next = proxy_handler if proxy_handler
 
     handlers.first
   end

08ad7b8d491dcdcf86ccaaf11a7375467b5f42d8
Anton Maminov <anton.linux@gmail.com>
2017-10-29 23:44:30 +0200

    allow to setup port in sample

--- samples/http_proxy.cr
+++ samples/http_proxy.cr
@@ -1,6 +1,15 @@
 require "../http/proxy"
+require "option_parser"
 
-server = HTTP::Proxy.new(8080, handlers: [
+port = 8080
+
+OptionParser.parse! do |opts|
+  opts.on("-p PORT", "--port PORT", "define port to run server") do |opt|
+    port = opt.to_i
+  end
+end
+
+server = HTTP::Proxy.new(port, handlers: [
   HTTP::LogHandler.new,
 ]) do |context|
   context.perform
@@ -10,5 +19,5 @@ server = HTTP::Proxy.new(8080, handlers: [
   # context.response.puts "Hello world! The time is #{Time.now}"
 end
 
-puts "Listening on http://127.0.0.1:8080"
+puts "Listening on http://127.0.0.1:#{port}"
 server.listen

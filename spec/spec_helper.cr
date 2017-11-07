0f9341ffbd4ec506b48f20e5dba60015dca0157e
Anton Maminov <anton.maminov@gmail.com>
2017-11-07 13:37:41 +0200

    add version

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,8 @@
 require "spec"
 require "../src/http_proxy"
+
+describe HTTP::Proxy do
+  it "should have version" do
+    (HTTP::Proxy::VERSION).should_not be_nil
+  end
+end
--- src/http_proxy.cr
+++ src/http_proxy.cr
@@ -4,3 +4,9 @@ require "base64"
 
 require "./http/proxy/server"
 require "./http/proxy/client"
+
+module HTTP
+  module Proxy
+    VERSION = "0.3.0"
+  end
+end

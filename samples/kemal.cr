f9b3144f4d6c857529df8255dc50c8e25d490700
Anton Maminov <anton.linux@gmail.com>
2018-03-27 14:37:28 +0300

    Kemal bug (#8)
    * reproduce issue #7
    
    * fix compatibility with kemal

---
+++ samples/kemal.cr
@@ -0,0 +1,18 @@
+require "../src/http_proxy"
+require "kemal"
+
+host = "127.0.0.1"
+port = 8080
+
+spawn do
+  server = HTTP::Proxy::Server.new(host, port, handlers: [
+    HTTP::LogHandler.new
+  ]) do |context|
+    context.request.headers.add("X-Forwarded-For", host)
+    context.perform
+  end
+  server.listen
+  puts "Listening on http://#{host}:#{port}"
+end
+
+Kemal.run

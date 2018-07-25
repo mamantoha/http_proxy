5a18b0207f68b86425048350b941ef3089145f4b
Anton Maminov <anton.linux@gmail.com>
2018-07-25 16:54:18 +0300

    update samples

--- samples/http_server.cr
+++ samples/http_server.cr
@@ -3,16 +3,11 @@ require "../src/http_proxy"
 host = "127.0.0.1"
 port = 8080
 
-# Issue
-server = HTTP::Proxy::Server.new(host, port)
+server = HTTP::Proxy::Server.new(host, port, handlers: [
+  HTTP::LogHandler.new,
+])
 
-# Works
-# server = HTTP::Proxy::Server.new(host, port, handlers: [
-#   HTTP::LogHandler.new,
-# ]) do |context|
-#   context.request.headers.add("X-Forwarded-For", host)
-#   context.perform
-# end
+server.bind_tcp(port)
 
 puts "Listening on http://#{server.host}:#{server.port}"
 server.listen

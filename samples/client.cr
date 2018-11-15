d58ddde25dd9181f406280c3de624115c583877f
Anton Maminov <anton.linux@gmail.com>
2018-11-15 21:36:02 +0200

    stop killing kitten (#14)
    * stop killing kitten
    
    * add ameba
    
    * add ameba to travis
    
    * change proxy connection options
    
    * add comment for proxy_basic_auth
    
    * rename samples

--- samples/client.cr
+++ samples/client.cr
@@ -5,19 +5,19 @@ host = "127.0.0.1"
 port = 8080
 
 OptionParser.parse! do |opts|
-  opts.on("-h HOST", "--host HOST", "define host to run server") do |opt|
+  opts.on("-h HOST", "--host HOST", "define host") do |opt|
     host = opt
   end
 
-  opts.on("-p PORT", "--port PORT", "define port to run server") do |opt|
+  opts.on("-p PORT", "--port PORT", "define port") do |opt|
     port = opt.to_i
   end
 end
 
 proxy_client = HTTP::Proxy::Client.new(host, port)
 
-puts "Make HTTP request w/o proxy"
-uri = URI.parse("http://httpbin.org")
+puts "Make HTTPs request w/o proxy"
+uri = URI.parse("https://httpbin.org")
 client = HTTP::Client.new(uri)
 response = client.get("/get")
 puts response.status_code
@@ -25,10 +25,9 @@ puts response.body
 
 puts "Make HTTPS request"
 uri = URI.parse("https://httpbin.org")
-response = HTTP::Client.new(uri) do |client|
-  client.set_proxy(proxy_client)
-  client.get("/get")
-end
+client = HTTP::Client.new(uri)
+client.set_proxy(proxy_client)
+response = client.get("/get")
 puts response.status_code
 puts response.body
 

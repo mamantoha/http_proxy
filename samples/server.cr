a9b8152afb65c56aa4d45c289c80b458e6121670
Anton Maminov <anton.linux@gmail.com>
2020-06-17 12:05:54 +0300

    change server sample

--- samples/server.cr
+++ samples/server.cr
@@ -3,6 +3,16 @@ require "../src/http_proxy"
 host = "127.0.0.1"
 port = 8080
 
+OptionParser.parse! do |opts|
+  opts.on("-h HOST", "--host HOST", "define host to run server") do |opt|
+    host = opt
+  end
+
+  opts.on("-p PORT", "--port PORT", "define port to run server") do |opt|
+    port = opt.to_i
+  end
+end
+
 server = HTTP::Proxy::Server.new(host, port, handlers: [
   HTTP::LogHandler.new,
 ])

690aa7041acd2038f5f548443bcf9221430652f5
Anton Maminov <anton.linux@gmail.com>
2026-02-05 17:46:34 +0200

    handle Proc(HTTP::Server::Context, Nil)

--- src/http/proxy/server/handler.cr
+++ src/http/proxy/server/handler.cr
@@ -3,12 +3,13 @@ require "./context"
 class HTTP::Proxy::Server::Handler
   include HTTP::Handler
 
-  property next : HTTP::Handler | HandlerProc | Nil
+  property next : HTTP::Handler | ContextProc | HandlerProc | Nil
 
   def call(context)
     HTTP::Proxy::Server::Context.new(context.request, context.response).perform
   end
 
+  alias ContextProc = HTTP::Server::Context ->
   alias HandlerProc = HTTP::Proxy::Server::Context ->
 end
 

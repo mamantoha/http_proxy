9b19801788f403e7e837a8299c3fc17ad99f4a5e
Anton Maminov <anton.linux@gmail.com>
2026-03-03 13:50:38 +0200

    Update server_mitm.cr

--- samples/server_mitm.cr
+++ samples/server_mitm.cr
@@ -3,9 +3,11 @@ require "option_parser"
 
 host = "127.0.0.1"
 port = 8080
-certificate_chain_path = "./mitm.crt"
-private_key_path = "./mitm.key"
+ca_certificate_path = "./mitm-ca.crt"
+ca_private_key_path = "./mitm-ca.key"
+certificate_cache_dir = "./.mitm-certs"
 upstream_insecure = false
+debug = false
 
 OptionParser.parse do |opts|
   opts.banner = "Usage: crystal run samples/server_mitm.cr -- [arguments]"
@@ -18,16 +20,24 @@ OptionParser.parse do |opts|
     port = opt.to_i
   end
 
-  opts.on("--cert PATH", "path to MITM certificate chain PEM") do |opt|
-    certificate_chain_path = opt
+  opts.on("--ca-cert PATH", "path to MITM CA certificate PEM (dynamic per-host cert mode)") do |opt|
+    ca_certificate_path = opt
   end
 
-  opts.on("--key PATH", "path to MITM private key PEM") do |opt|
-    private_key_path = opt
+  opts.on("--ca-key PATH", "path to MITM CA private key PEM (dynamic per-host cert mode)") do |opt|
+    ca_private_key_path = opt
+  end
+
+  opts.on("--cache-dir PATH", "directory to store generated host certificates") do |opt|
+    certificate_cache_dir = opt
   end
 
   opts.on("--upstream-insecure", "disable upstream TLS verification") do
     upstream_insecure = true
+  end
+
+  opts.on("--debug", "enable verbose MITM request/response debug output") do
+    debug = true
   end
 end
 
@@ -36,15 +46,20 @@ server = HTTP::Proxy::Server.new(handlers: [
 ])
 
 server.mitm = HTTP::Proxy::Server::MITMConfig.new(
-  certificate_chain_path: certificate_chain_path,
-  private_key_path: private_key_path,
+  ca_certificate_path: ca_certificate_path,
+  ca_private_key_path: ca_private_key_path,
+  certificate_cache_dir: certificate_cache_dir,
   upstream_insecure: upstream_insecure,
+  debug: debug,
 )
 
 address = server.bind_tcp(host, port)
 puts "Listening on http://#{address}"
 puts "MITM mode enabled (CONNECT HTTP/1.1 MVP)"
-puts "Certificate: #{certificate_chain_path}"
-puts "Private key: #{private_key_path}"
+puts "Certificate mode: dynamic per-host"
+puts "CA certificate: #{ca_certificate_path}"
+puts "CA private key: #{ca_private_key_path}"
+puts "Certificate cache dir: #{certificate_cache_dir}"
 puts "Upstream TLS verification: #{upstream_insecure ? "DISABLED" : "ENABLED"}"
+puts "MITM debug output: #{debug ? "ENABLED" : "DISABLED"}"
 server.listen
